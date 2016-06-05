b0539dfe47056dc3f6fd31de36d2fa3f4a6cc5fe
Jose Mejias <jota@MacBook-Pro-de-Jose.local>
2016-06-05 11:22:48 +0200

    init repo

---
+++ index.js
@@ -0,0 +1,42 @@
+// SERVER
+const express = require('express');
+const http =  require('http');
+const app = express();
+const io = require('socket.io')(http.createServer(app));
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', (process.env.PORT || 3000));
+
+//SERVER
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/' + 'index.html');
+});
+
+// POST http://localhost:8080/api/users
+// parameters sent with 
+app.post('/api/message', function(req, res) {
+    var isOriginal = req.body.original; //boolean
+    var text = req.body.text; //string
+
+    if (isOriginal == 'true') {
+        io.emit('original text', text);
+    } else if (isOriginal == 'false') {
+        io.emit('translated text', text);
+    }
+
+    res.send('OK');
+});
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
